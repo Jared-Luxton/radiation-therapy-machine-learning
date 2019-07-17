957364cc17a3f22237316dc554f59c6e27be6c46
Jared Luxton <jaredjamesluxton@gmail.com>
2019-07-17 09:45:08 -0600

    rain cloud plots, refactoring notebook contents

--- telomere_methods_rad_patient.py
+++ telomere_methods_rad_patient.py
@@ -305,6 +305,12 @@ def histogram_stylizer_divyBins_byQuartile(fig, axs, n_bins, astroDF, astroquart
         N, bins, patches = axs[axsNUMone,axsNUMtwo].hist(astroarray, bins=n_bins, range=(0, 400), edgecolor='black')
 
         for a in range(len(patches)):
+#             print(bins)
+#             [  0.   8.  16.  24.  32.  40.  48.  56.  64.  72.  80.  88.  96. 104.
+        #  112. 120. 128. 136. 144. 152. 160. 168. 176. 184. 192. 200. 208. 216.
+        #  224. 232. 240. 248. 256. 264. 272. 280. 288. 296. 304. 312. 320. 328.
+        #  336. 344. 352. 360. 368. 376. 384. 392. 400.]
+
             if bins[a] <= np.quantile(astroquartile, 0.25):
                 patches[a].set_facecolor('#fdff38')
 
